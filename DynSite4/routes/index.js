a3460e087c7c9dc09af2d680eac9cc1ad6909e48
ajlopez <webmaster@ajlopez.com>
2014-06-25 09:36:14 -0300

    Dynamic site in JSON, first pages

--- DynSite4/routes/index.js
+++ DynSite4/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var site = require('../site.json');
+
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express 4' });
-});
+router.get('/', makeAction('home'));
+
+for (var name in site.pages)
+    router.get('/' + name, makeAction(name));
+
+function makeAction(name) {
+    return function(req, res) { process(req, res, name); }
+}
+
+function process(req, res, name) {
+    var page = site.pages[name];
+    
+    res.render('page', { title: page.title, text: page.text });
+}
 
 module.exports = router;
